f3f7583319663b5fca4e81a2efbe3c6c4f00292d
Alex Pickering <alex.pickering15@gmail.com>
2020-09-01 08:53:28 -0400

    Added more properties to dota spider. Also Beigel vertically aligned a bunch of your code.

--- dota_spider.py
+++ dota_spider.py
@@ -29,17 +29,14 @@ class DotaSpider(scrapy.Spider):
                 break
 
     def parse_hero(self, response):
-        print("DIFFERENT RESPONSE START")
-        print(response)
-        print(response.meta)
-        print("DIFFERENT RESPONSE END")
 
-        for chunk in response.xpath("/html/body/div[2]/div[3]/div[1]/div[3]/div[4]/div/table[1]/tbody"):
+        for chunk in response.xpath("//table[contains(@class, 'infobox')]/tbody"):
             base_chunk = chunk.xpath("tr[1]/th/div[2]")
             if isinstance(base_chunk, list) and len(base_chunk) == 1:
                 base_chunk = base_chunk[0]
             else:
                 raise Exception
+
             base_strength       = base_chunk.xpath("div[4]/b/text()").get()
             strength_growth     = base_chunk.xpath("div[4]/text()").get()[3:]
             base_agility        = base_chunk.xpath("div[5]/b/text()").get()
@@ -52,6 +49,7 @@ class DotaSpider(scrapy.Spider):
                 lvl_chunk = lvl_chunk[0]
             else:
                 raise Exception
+
             health = {
                         0 : lvl_chunk.xpath("tr[2]/td[1]/text()").get().strip(),
                         1 : lvl_chunk.xpath("tr[2]/td[2]/text()").get().strip(),
@@ -94,12 +92,19 @@ class DotaSpider(scrapy.Spider):
                         25: lvl_chunk.xpath("tr[7]/td[4]/text()").get().strip(),
                         30: lvl_chunk.xpath("tr[7]/td[5]/text()").get().strip()
                     } 
-            damage = {
-                        0 : lvl_chunk.xpath("tr[8]/td[1]/text()").get().strip(),
-                        1 : lvl_chunk.xpath("tr[8]/td[2]/text()").get().strip(),
-                        15: lvl_chunk.xpath("tr[8]/td[3]/text()").get().strip(),
-                        25: lvl_chunk.xpath("tr[8]/td[4]/text()").get().strip(),
-                        30: lvl_chunk.xpath("tr[8]/td[5]/text()").get().strip()
+            damage_low = {
+                        0 : lvl_chunk.xpath("tr[8]/td[1]/text()").get().strip().split('‒')[0],
+                        1 : lvl_chunk.xpath("tr[8]/td[2]/text()").get().strip().split('‒')[0],
+                        15: lvl_chunk.xpath("tr[8]/td[3]/text()").get().strip().split('‒')[0],
+                        25: lvl_chunk.xpath("tr[8]/td[4]/text()").get().strip().split('‒')[0],
+                        30: lvl_chunk.xpath("tr[8]/td[5]/text()").get().strip().split('‒')[0]
+                    }
+            damage_high = {
+                        0 : lvl_chunk.xpath("tr[8]/td[1]/text()").get().strip().split('‒')[1],
+                        1 : lvl_chunk.xpath("tr[8]/td[2]/text()").get().strip().split('‒')[1],
+                        15: lvl_chunk.xpath("tr[8]/td[3]/text()").get().strip().split('‒')[1],
+                        25: lvl_chunk.xpath("tr[8]/td[4]/text()").get().strip().split('‒')[1],
+                        30: lvl_chunk.xpath("tr[8]/td[5]/text()").get().strip().split('‒')[1]
                     } 
 
             other_stats_chunk = chunk.xpath("tr[3]/td/table/tbody")
@@ -107,68 +112,66 @@ class DotaSpider(scrapy.Spider):
                 other_stats_chunk = other_stats_chunk[0]
             else:
                 raise Exception
-            magic_resistance = other_stats_chunk.xpath("tr[1]/td/text()").get().strip()
-            movement_speed = other_stats_chunk.xpath("tr[2]/td/text()").get().strip()
-            attach_speed = other_stats_chunk.xpath("tr[3]/td/text()").get().strip()
-            turn_rate = other_stats_chunk.xpath("tr[4]/td/text()").get().strip()
-            vision_range = "(Day/Night): " + \
-                other_stats_chunk.xpath("tr[5]/td/span[1]/text()").get() + "/" + \
-                other_stats_chunk.xpath("tr[5]/td/span[2]/text()").get()
-            attack_range = other_stats_chunk.xpath("tr[6]/td/span/@title").get().strip() + \
-                    ": " + other_stats_chunk.xpath("tr[6]/td/span/text()").get().strip()
-            projectile_speed = other_stats_chunk.xpath("tr[7]/td/text()").get().strip()
-            attack_animation = "(Atk Point/Atk Backswing): " + \
-                other_stats_chunk.xpath("tr[8]/td/span[1]/text()").get() + "+" + \
-                other_stats_chunk.xpath("tr[8]/td/span[2]/text()").get() 
-            base_attack_time = other_stats_chunk.xpath("tr[9]/td/text()").get().strip()
-            damage_block = other_stats_chunk.xpath("tr[10]/td/text()").get().strip()
-            collision_size = other_stats_chunk.xpath("tr[11]/td/text()").get().strip()
-            legs = other_stats_chunk.xpath("tr[12]/td/text()").get().strip()
-            gib_type = other_stats_chunk.xpath("tr[13]/td/text()").get().strip()
 
-            health_set = DotaSpider.stats_across_levels(health, name='health')
-            health_regen_set = DotaSpider.stats_across_levels(health_regen, name='health_regen')
+            magic_resistance           = other_stats_chunk.xpath("tr[1]/td/text()").get().strip()
+            movement_speed             = other_stats_chunk.xpath("tr[2]/td/text()").get().strip()
+            attack_speed               = other_stats_chunk.xpath("tr[3]/td/text()").get().strip()
+            turn_rate                  = other_stats_chunk.xpath("tr[4]/td/text()").get().strip()
+            vision_range_day           = other_stats_chunk.xpath("tr[5]/td/span[1]/text()").get()
+            vision_range_night         = other_stats_chunk.xpath("tr[5]/td/span[2]/text()").get()
+            attack_type                = other_stats_chunk.xpath("tr[6]/td/span/@title").get().strip()
+            attack_range               = other_stats_chunk.xpath("tr[6]/td/span/text()").get().strip()
+            projectile_speed           = other_stats_chunk.xpath("tr[7]/td/text()").get().strip()
+            attack_animation_point     = other_stats_chunk.xpath("tr[8]/td/span[1]/text()").get()
+            attack_animation_backswing = other_stats_chunk.xpath("tr[8]/td/span[2]/text()").get()
+            base_attack_time           = other_stats_chunk.xpath("tr[9]/td/text()").get().strip()
+            damage_block               = other_stats_chunk.xpath("tr[10]/td/text()").get().strip()
+            collision_size             = other_stats_chunk.xpath("tr[11]/td/text()").get().strip()
+            legs                       = other_stats_chunk.xpath("tr[12]/td/text()").get().strip()
+            gib_type                   = other_stats_chunk.xpath("tr[13]/td/text()").get().strip()
 
-
+            health_set             = DotaSpider.stats_across_levels(health,             name='health')
+            health_regen_set       = DotaSpider.stats_across_levels(health_regen,       name='health_regen')
+            mana_set               = DotaSpider.stats_across_levels(mana,               name='mana')
+            mana_regen_set         = DotaSpider.stats_across_levels(mana_regen,         name='mana_regen')
+            armor_set              = DotaSpider.stats_across_levels(armor,              name='armor')
+            attacks_per_second_set = DotaSpider.stats_across_levels(attacks_per_second, name='attacks_per_second')
+            damage_low_set         = DotaSpider.stats_across_levels(damage_low,         name='damage_low')
+            damage_high_set        = DotaSpider.stats_across_levels(damage_high,        name='damage_high')
 
         yield {
-                'hero': response.meta['hero'],
-                'image': response.meta['image'],
-                'base_strength': base_strength,
-                'strength_growth': strength_growth,
-                'base_agility': base_agility,
-                'agility_growth': agility_growth,
-                'base_intelligence': base_intelligence,
+                'hero':                response.meta['hero'],
+                'image':               response.meta['image'],
+                'base_strength':       base_strength,
+                'strength_growth':     strength_growth,
+                'base_agility':        base_agility,
+                'agility_growth':      agility_growth,
+                'base_intelligence':   base_intelligence,
                 'intelligence_growth': intelligence_growth,
                 **health_set,
                 **health_regen_set,
-                # TODO(apick): add other lvl-dependent stats on 129 and below
-
-                #'health_0': health[0],
-                #'health_1': health[1],
-                #'health_15': health[15],
-                #'health_25': health[25],
-                #'health_30': health[30],
-                #'health_regen': health_regen,
-                #'mana': mana,
-                #'mana_regen': mana_regen,
-                #'armor': armor,
-                #'attacks_per_second': attacks_per_second,
-                #'damage': damage,
-
-                'magic_resistance': magic_resistance,
-                'movement_speed': movement_speed,
-                'attach_speed': attach_speed,
-                'turn_rate': turn_rate,
-                'vision_range': vision_range,
-                'attack_range': attack_range,
-                'projectile_speed': projectile_speed,
-                'attack_animation': attack_animation,
-                'base_attack_time': base_attack_time,
-                'damage_block': damage_block,
-                'collision_size': collision_size,
-                'legs': legs,
-                'gib_type': gib_type
+                **mana_set,
+                **mana_regen_set,
+                **armor_set,
+                **attacks_per_second_set,
+                **damage_low_set,
+                **damage_high_set,
+                'magic_resistance':           magic_resistance,
+                'movement_speed':             movement_speed,
+                'attack_speed':               attack_speed,
+                'turn_rate':                  turn_rate,
+                'vision_range_day':           vision_range_day,
+                'vision_range_night':         vision_range_night,
+                'attack_type':                attack_type,
+                'attack_range':               attack_range,
+                'projectile_speed':           projectile_speed,
+                'attack_animation_point':     attack_animation_point,
+                'attack_animation_backswing': attack_animation_backswing,
+                'base_attack_time':           base_attack_time,
+                'damage_block':               damage_block,
+                'collision_size':             collision_size,
+                'legs':                       legs,
+                'gib_type':                   gib_type
               }
 
 
