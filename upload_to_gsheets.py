6dade1a8127df4107c96bec3be36d9afacd803cc
Alex Pickering <alex.pickering15@gmail.com>
2020-09-03 17:42:27 -0400

    Add prelim data bounds for Gsheets(A1)

--- upload_to_gsheets.py
+++ upload_to_gsheets.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import argparse
+import csv
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
@@ -20,18 +21,38 @@ def main():
 
     spreadsheet = client.open('DotA 2 Hero Stats')
 
-
     # temporarily commented out: upload overwrites formatting
     # Upload part
     with open(args.csv, 'r') as file_obj:
-        content = file_obj.read()
+        edited_file_obj = edit_data(file_obj)
+        content = edited_file_obj.read()
         client.import_csv(spreadsheet.id, data=content)
 
 
     sh = spreadsheet.sheet1
 
+    # TODO(apick): 
+    # 1. cull headers
+    # 2. make lvl-dependent stats via formulas
+
+    # B1:AE1 = lvl request cells
+    # starts = [ (x * 119) + 3 for x in range(29) ]
+    # ends   = [ (x * 119) + 121 for x in range(29) ]
+    # per_lvl_blocks = [str(x) + ':' + str(y) for x, y in zip(starts, ends)]
+    
+
+
+
+
     # formatting cells
-    sh.format('A1:BK1', {'textFormat': {'bold': True}})
+    sh.format('A1:BQ1', {'textFormat': {'bold': True}})
+    sh.sort((1, 'asc'), range='A2:BQ120')
+
+
+def edit_data(csvfile):
+    csvreader = csv.reader(csvfile)
+
+
 
 
 if  __name__ == '__main__':
