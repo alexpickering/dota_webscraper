f5da4ddf0afe5e8fd852697ffac87ba74fd6eb2e
Alex Beigel <beigel.alex@gmail.com>
2020-10-31 20:34:48 -0400

    Formatted gsheets_handle_stats.py. Also alphabetized requirements.txt

--- dota_webscraper/gsheets_handle_stats.py
+++ dota_webscraper/gsheets_handle_stats.py
@@ -25,7 +25,14 @@ def calc_all_heroes(datafile_name, all_lvl, outfile_name):
         # gsheets is 1-indexed, 1 is Level:, all_lvl, 2 is header rows, heroes start at 3
         new_index = str(3)
 
-        headers = {'Hero': '$A', 'Primary': '$B', 'Strength': '$C', 'Agility': '$D','Intelligence': '$E', 'Attribute Total': '$F'}
+        headers = {
+                'Hero':            '$A',
+                'Primary':         '$B',
+                'Strength':        '$C',
+                'Agility':         '$D',
+                'Intelligence':    '$E',
+                'Attribute Total': '$F'
+        }
 
         hero_list = df.hero.values
 
@@ -35,60 +42,60 @@ def calc_all_heroes(datafile_name, all_lvl, outfile_name):
             dict1 = {}
 
             # store frequently used variables for clarity
-            primary, = str(df.at[i,'primary_attribute']),
-            base_strength = str(df.at[i,'base_strength'])
-            strength_growth = str(df.at[i,'strength_growth'])
-            base_agility = str(df.at[i,'base_agility'])
-            agility_growth = str(df.at[i,'agility_growth'])
-            base_intelligence = str(df.at[i,'base_intelligence'])
+            primary,            = str(df.at[i,'primary_attribute']),
+            base_strength       = str(df.at[i,'base_strength'])
+            strength_growth     = str(df.at[i,'strength_growth'])
+            base_agility        = str(df.at[i,'base_agility'])
+            agility_growth      = str(df.at[i,'agility_growth'])
+            base_intelligence   = str(df.at[i,'base_intelligence'])
             intelligence_growth = str(df.at[i,'intelligence_growth'])
-            base_health_regen = str(df.at[i,'health_regen_0'])
-            base_mana = str(df.at[i,'mana_0'])
-            base_mana_regen = str(df.at[i,'mana_regen_0'])
-            base_armor = str(df.at[i,'armor_0'])
-            attack_speed = str(df.at[i,'attack_speed'])
-            base_attack_time = str(df.at[i,'base_attack_time'])
-            base_damage_low = str(df.at[i,'damage_low_0'])
-            base_damage_high = str(df.at[i,'damage_high_0'])
+            base_health_regen   = str(df.at[i,'health_regen_0'])
+            base_mana           = str(df.at[i,'mana_0'])
+            base_mana_regen     = str(df.at[i,'mana_regen_0'])
+            base_armor          = str(df.at[i,'armor_0'])
+            attack_speed        = str(df.at[i,'attack_speed'])
+            base_attack_time    = str(df.at[i,'base_attack_time'])
+            base_damage_low     = str(df.at[i,'damage_low_0'])
+            base_damage_high    = str(df.at[i,'damage_high_0'])
 
             # cell reference in A1 (ex. $D3)
-            primary_cell = ''.join([headers[primary.title()],new_index])
-            strength_cell = ''.join([headers['Strength'],new_index])
-            agility_cell = ''.join([headers['Agility'],new_index])
+            primary_cell      = ''.join([headers[primary.title()],new_index])
+            strength_cell     = ''.join([headers['Strength'],new_index])
+            agility_cell      = ''.join([headers['Agility'],new_index])
             intelligence_cell = ''.join([headers['Intelligence'],new_index])
-            all_lvl_cell = '$B$1'
+            all_lvl_cell      = '$B$1'
 
             # cell values defined
-            strength_cell_val = ''.join(['=',base_strength,'+',strength_growth,'*(',all_lvl_cell,'-1)'])
-            agility_cell_val = ''.join(['=',base_agility,'+',agility_growth,'*(',all_lvl_cell,'-1)'])
-            intelligence_cell_val = ''.join(['=',base_intelligence,'+',intelligence_growth,'*(',all_lvl_cell,'-1)'])
-            total_attr_val = ''.join(['=',strength_cell,'+',agility_cell,'+',intelligence_cell])
-            health = ''.join(['=200+(rounddown(',strength_cell,')*20)'])
-            health_regen = ''.join(['=',base_health_regen,'+(0.1*',strength_cell,')'])
-            mana = ''.join(['=',base_mana,'+(12*rounddown(',intelligence_cell,'))'])
-            mana_regen = ''.join(['=',base_mana_regen,'+(0.05*',intelligence_cell,')'])
-            armor = ''.join(['=round(',base_armor,'+((1/6)*',agility_cell,'),2)'])
-            attacks_per_second = ''.join(['=round(((',attack_speed,'+',agility_cell,')*0.01)/',base_attack_time,',2)'])
-            damage_low = ''.join(['=',base_damage_low,'+rounddown(',primary_cell,')'])
-            damage_high = ''.join(['=',base_damage_high,'+rounddown(',primary_cell,')'])
+            strength_cell_val     = ''.join([' = ',base_strength,'+',strength_growth,'*(',all_lvl_cell,'-1)'])
+            agility_cell_val      = ''.join([' = ',base_agility,'+',agility_growth,'*(',all_lvl_cell,'-1)'])
+            intelligence_cell_val = ''.join([' = ',base_intelligence,'+',intelligence_growth,'*(',all_lvl_cell,'-1)'])
+            total_attr_val        = ''.join([' = ',strength_cell,'+',agility_cell,'+',intelligence_cell])
+            health                = ''.join([' = 200+(rounddown(',strength_cell,')*20)'])
+            health_regen          = ''.join([' = ',base_health_regen,'+(0.1*',strength_cell,')'])
+            mana                  = ''.join([' = ',base_mana,'+(12*rounddown(',intelligence_cell,'))'])
+            mana_regen            = ''.join([' = ',base_mana_regen,'+(0.05*',intelligence_cell,')'])
+            armor                 = ''.join([' = round(',base_armor,'+((1/6)*',agility_cell,'),2)'])
+            attacks_per_second    = ''.join([' = round(((',attack_speed,'+',agility_cell,')*0.01)/',base_attack_time,',2)'])
+            damage_low            = ''.join([' = ',base_damage_low,'+rounddown(',primary_cell,')'])
+            damage_high           = ''.join([' = ',base_damage_high,'+rounddown(',primary_cell,')'])
 
 
             # assign values to temporary dictionary
             dict1.update({
-                    'Hero': hero,
-                    'Primary': primary,
-                    'Strength': strength_cell_val,
-                    'Agility': agility_cell_val,
-                    'Intelligence': intelligence_cell_val,
+                    'Hero':             hero,
+                    'Primary':          primary,
+                    'Strength':         strength_cell_val,
+                    'Agility':          agility_cell_val,
+                    'Intelligence':     intelligence_cell_val,
                     'Total Attributes': total_attr_val,
-                    'Health': health,
-                    'Health Regen': health_regen,
-                    'Mana': mana,
-                    'Mana Regen': mana_regen,
-                    'Armor': armor,
-                    'Attacks/Second': attacks_per_second,
-                    'Damage Low': damage_low,
-                    'Damage High': damage_high
+                    'Health':           health,
+                    'Health Regen':     health_regen,
+                    'Mana':             mana,
+                    'Mana Regen':       mana_regen,
+                    'Armor':            armor,
+                    'Attacks/Second':   attacks_per_second,
+                    'Damage Low':       damage_low,
+                    'Damage High':      damage_high
             })
 
             rows.append(dict1)
@@ -117,67 +124,75 @@ def calc_requested_heroes(datafile_name, request_dict,outfile_name):
         # track index of requested heroes
         new_index = str(2)
 
-        headers = {'Hero':'$A', 'Level': '$B', 'Primary': '$C', 'Strength': '$D','Agility':'$E','Intelligence':'$F', 'Attribute Total': '$G'}
+        headers = {
+                'Hero':            '$A',
+                'Level':           '$B',
+                'Primary':         '$C',
+                'Strength':        '$D',
+                'Agility':         '$E',
+                'Intelligence':    '$F',
+                'Attribute Total': '$G'
+        }
 
         for hero, lvls in request_dict.items():
             i = df.index[df['hero'] == hero].tolist()[0]
             for lvl in lvls:
                 dict1 = {}
                 # store frequently used variables for clarity
-                primary, = str(df.at[i,'primary_attribute']),
-                base_strength = str(df.at[i,'base_strength'])
-                strength_growth = str(df.at[i,'strength_growth'])
-                base_agility = str(df.at[i,'base_agility'])
-                agility_growth = str(df.at[i,'agility_growth'])
-                base_intelligence = str(df.at[i,'base_intelligence'])
+                primary,            = str(df.at[i,'primary_attribute']),
+                base_strength       = str(df.at[i,'base_strength'])
+                strength_growth     = str(df.at[i,'strength_growth'])
+                base_agility        = str(df.at[i,'base_agility'])
+                agility_growth      = str(df.at[i,'agility_growth'])
+                base_intelligence   = str(df.at[i,'base_intelligence'])
                 intelligence_growth = str(df.at[i,'intelligence_growth'])
-                base_health_regen = str(df.at[i,'health_regen_0'])
-                base_mana = str(df.at[i,'mana_0'])
-                base_mana_regen = str(df.at[i,'mana_regen_0'])
-                base_armor = str(df.at[i,'armor_0'])
-                attack_speed = str(df.at[i,'attack_speed'])
-                base_attack_time = str(df.at[i,'base_attack_time'])
-                base_damage_low = str(df.at[i,'damage_low_0'])
-                base_damage_high = str(df.at[i,'damage_high_0'])
+                base_health_regen   = str(df.at[i,'health_regen_0'])
+                base_mana           = str(df.at[i,'mana_0'])
+                base_mana_regen     = str(df.at[i,'mana_regen_0'])
+                base_armor          = str(df.at[i,'armor_0'])
+                attack_speed        = str(df.at[i,'attack_speed'])
+                base_attack_time    = str(df.at[i,'base_attack_time'])
+                base_damage_low     = str(df.at[i,'damage_low_0'])
+                base_damage_high    = str(df.at[i,'damage_high_0'])
 
                 # cell reference in A1 (ex. $D3)
-                primary_cell = ''.join([headers[primary.title()],new_index])
-                strength_cell = ''.join([headers['Strength'],new_index])
-                agility_cell = ''.join([headers['Agility'],new_index])
+                primary_cell      = ''.join([headers[primary.title()],new_index])
+                strength_cell     = ''.join([headers['Strength'],new_index])
+                agility_cell      = ''.join([headers['Agility'],new_index])
                 intelligence_cell = ''.join([headers['Intelligence'],new_index])
 
                 # cell values defined
-                strength_cell_val = ''.join(['=',base_strength,'+',strength_growth,'*(',headers['Level'],new_index,'-1)'])
-                agility_cell_val = ''.join(['=',base_agility,'+',agility_growth,'*(',headers['Level'],new_index,'-1)'])
-                intelligence_cell_val = ''.join(['=',base_intelligence,'+',intelligence_growth,'*(',headers['Level'],new_index,'-1)'])
-                total_attr_val = ''.join(['=',strength_cell,'+',agility_cell,'+',intelligence_cell])
-                health = ''.join(['=200+(rounddown(',strength_cell,')*20)'])
-                health_regen = ''.join(['=',base_health_regen,'+(0.1*',strength_cell,')'])
-                mana = ''.join(['=',base_mana,'+(12*rounddown(',intelligence_cell,'))'])
-                mana_regen = ''.join(['=',base_mana_regen,'+(0.05*',intelligence_cell,')'])
-                armor = ''.join(['=',base_armor,'+((1/6)*',agility_cell,')'])
-                attacks_per_second = ''.join(['=((',attack_speed,'+',agility_cell,')*0.01)/',base_attack_time])
-                damage_low = ''.join(['=',base_damage_low,'+rounddown(',primary_cell,')'])
-                damage_high = ''.join(['=',base_damage_high,'+rounddown(',primary_cell,')'])
+                strength_cell_val     = ''.join([' = ',base_strength,'+',strength_growth,'*(',headers['Level'],new_index,'-1)'])
+                agility_cell_val      = ''.join([' = ',base_agility,'+',agility_growth,'*(',headers['Level'],new_index,'-1)'])
+                intelligence_cell_val = ''.join([' = ',base_intelligence,'+',intelligence_growth,'*(',headers['Level'],new_index,'-1)'])
+                total_attr_val        = ''.join([' = ',strength_cell,'+',agility_cell,'+',intelligence_cell])
+                health                = ''.join([' = 200+(rounddown(',strength_cell,')*20)'])
+                health_regen          = ''.join([' = ',base_health_regen,'+(0.1*',strength_cell,')'])
+                mana                  = ''.join([' = ',base_mana,'+(12*rounddown(',intelligence_cell,'))'])
+                mana_regen            = ''.join([' = ',base_mana_regen,'+(0.05*',intelligence_cell,')'])
+                armor                 = ''.join([' = ',base_armor,'+((1/6)*',agility_cell,')'])
+                attacks_per_second    = ''.join([' = ((',attack_speed,'+',agility_cell,')*0.01)/',base_attack_time])
+                damage_low            = ''.join([' = ',base_damage_low,'+rounddown(',primary_cell,')'])
+                damage_high           = ''.join([' = ',base_damage_high,'+rounddown(',primary_cell,')'])
 
 
                 # assign values to temporary dictionary
                 dict1.update({
-                        'Hero': ''.join(['="',hero,'_"&',headers['Level'],new_index]),
-                        'Level': lvl,
-                        'Primary': primary,
-                        'Strength': strength_cell_val,
-                        'Agility': agility_cell_val,
-                        'Intelligence': intelligence_cell_val,
+                        'Hero':             ''.join(['="',hero,'_"&',headers['Level'],new_index]),
+                        'Level':            lvl,
+                        'Primary':          primary,
+                        'Strength':         strength_cell_val,
+                        'Agility':          agility_cell_val,
+                        'Intelligence':     intelligence_cell_val,
                         'Total Attributes': total_attr_val,
-                        'Health': health,
-                        'Health Regen': health_regen,
-                        'Mana': mana,
-                        'Mana Regen': mana_regen,
-                        'Armor': armor,
-                        'Attacks/Second': attacks_per_second,
-                        'Damage Low': damage_low,
-                        'Damage High': damage_high
+                        'Health':           health,
+                        'Health Regen':     health_regen,
+                        'Mana':             mana,
+                        'Mana Regen':       mana_regen,
+                        'Armor':            armor,
+                        'Attacks/Second':   attacks_per_second,
+                        'Damage Low':       damage_low,
+                        'Damage High':      damage_high
                 })
 
                 rows.append(dict1)
